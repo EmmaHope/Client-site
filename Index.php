71c479d329045c1be6ffb33e38a10266f9913319
IOI655321sorrento <jordan.zapata@student.cart.org>
2019-05-08 14:02:20 -0700

    First commit

---
+++ Index.php
@@ -0,0 +1,18 @@
+<?php 
+$a = "<html>
+    <head>
+        <title>Client Site</title>
+        <meta charset='utf-8' lang='en'>
+        <link href='Index.css' rel='stylesheet'> 
+    </head>
+    <body>
+        <div class='nav'>
+            <button class='button'><a href='Index.php'>Home</a></button>
+        </div>
+        <p>Hello World!</p>
+    </body>
+</html>";
+
+echo "$a";
+
+    ?>
