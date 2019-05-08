1aedb2207c7cbf59657a8c2b823c33bebaa98255
IOI655321sorrento <jordan.zapata@student.cart.org>
2019-05-08 15:18:59 -0700

    nav bar

--- Index.php
+++ Index.php
@@ -8,8 +8,14 @@ $a = "<html>
     <body>
         <div class='nav'>
             <button class='button'><a href='Index.php'>Home</a></button>
+            <button class='button'><a href='Index2.php'>About</a></button>
+            <button class='button'><a href='Index3.php'>Collage</a></button>
+            <button class='button'><a href='Index4.php'>Contact</a></button>
         </div>
-        <p>Hello World!</p>
+        <div class='row1'>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique. Dui ut ornare lectus sit. Libero id faucibus nisl tincidunt eget nullam non. Interdum varius sit amet mattis vulputate enim nulla. Eu ultrices vitae auctor eu augue ut lectus. Tellus orci ac auctor augue mauris. Tortor condimentum lacinia quis vel eros donec ac odio tempor. Arcu risus quis varius quam. Egestas pretium aenean pharetra magna. Gravida cum sociis natoque penatibus et magnis dis parturient. Sed vulputate odio ut enim blandit volutpat maecenas volutpat.</p>
+        <img src='PICS/placeholder' alt='placeholder' height='250' width='250'>
+        </div>
     </body>
 </html>";
 
